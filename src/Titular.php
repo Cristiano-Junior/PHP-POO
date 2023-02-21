e25299bcfd90232bf26c428e6e25aee6a2139c52
Cristiano Junior <cristianojunior120@gmail.com>
2023-02-21 17:58:02 -0300

    finalizado aula sobre Herança

--- src/Titular.php
+++ src/Titular.php
@@ -1,38 +1,17 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private CPF $cpf;
-    private $nome;
     private Endereco $endereco;
 
     public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
-    }
-
-    public function getCpf(): string
-    {
-        return $this->cpf->getDocumento();
-    }
-
-    public function getNome(): string
-    {
-        return $this->nome;
+        parent::__construct($cpf, $nome);
+        $this->$endereco = $endereco;
     }
 
     public function getEndereco() : Endereco
     {
         return $this->endereco;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
     }
 }
